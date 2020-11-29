2dbe7780bf10c55212918a05545df5f6e4653603
monthalcantara <montival_junior@yahoo.com.br>
2020-11-29 10:52:42 -0300

    criacao do projeto

---
+++ src/main/java/io/github/monthalcantara/springboot2essentials/Springboot2EssentialsApplication.java
@@ -0,0 +1,13 @@
+package io.github.monthalcantara.springboot2essentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2EssentialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2EssentialsApplication.class, args);
+	}
+
+}
