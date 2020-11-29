a6f0c5090c78e37f9276764495a572473c9a1839
monthalcantara <montival_junior@yahoo.com.br>
2020-11-29 19:14:46 -0300

    Modificado classe de exposição de exceptions

--- src/main/java/io/github/monthalcantara/springboot2essentials/compartilhado/exceptions/CapturaErrosAplicacaoController.java
+++ src/main/java/io/github/monthalcantara/springboot2essentials/compartilhado/exceptions/CapturaErrosAplicacaoController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.validation.ConstraintViolationException;
+import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,20 +21,24 @@ public class CapturaErrosAplicacaoController {
     @ExceptionHandler(IllegalStateException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErroApi handelResponseStatusException(IllegalStateException e) {
+        String title = "Desculpa, estamos com alguns problemas internos mas logo logo tudo será normalizado";
+
         logger.error(e.getMessage());
-        return new ErroApi("Desculpa, estamos com alguns problemas internos mas logo logo tudo será normalizado");
+        return new ErroApi(title, HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getClass().getName(), Arrays.asList(e.getMessage()), LocalDateTime.now());
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErroApi handelConstraintViolationException(ConstraintViolationException e) {
-        return new ErroApi(e.getMessage());
+        String title = "Bad Request, verifique a documentação";
+        return new ErroApi(title, HttpStatus.BAD_REQUEST.value(), e.getClass().getName(), Arrays.asList(e.getMessage()), LocalDateTime.now());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErroApi handelMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        String title = "Bad Request, verifique a documentação";
         List<String> listaErros = e.getBindingResult().getAllErrors().stream().map(err -> err.getDefaultMessage()).collect(Collectors.toList());
-        return new ErroApi(listaErros);
+        return new ErroApi(title, HttpStatus.BAD_REQUEST.value(), e.getClass().getName(), listaErros, LocalDateTime.now());
     }
 }
--- src/main/java/io/github/monthalcantara/springboot2essentials/compartilhado/exceptions/ErroApi.java
+++ src/main/java/io/github/monthalcantara/springboot2essentials/compartilhado/exceptions/ErroApi.java
@@ -1,21 +1,42 @@
 package io.github.monthalcantara.springboot2essentials.compartilhado.exceptions;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class ErroApi {
 
+    private String title;
+    private int Status;
+    private String details;
+    private LocalDateTime timestamp;
     private List<String> erros = new ArrayList<>();
 
-    public ErroApi(List<String> erros) {
+    public ErroApi(String title, int status, String details, List<String> erros, LocalDateTime timestamp) {
+        this.title = title;
+        Status = status;
+        this.details = details;
+        this.timestamp = timestamp;
         this.erros = erros;
     }
-    public ErroApi(String erro) {
-            this.erros = Arrays.asList(erro);
-        }
 
     public List<String> getErros() {
         return erros;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getStatus() {
+        return Status;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
     }
 }
